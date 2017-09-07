e42b9dc6b6b208e63e063722f70ff1612a3142c2
TonyStark10006 <stvofy@gmail.com>
2017-09-07 12:53:16 +0800

    修正用户主动取消订单功能的一些问题

--- app/Model/Management/Stock/StockRollBackModel.php
+++ app/Model/Management/Stock/StockRollBackModel.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Session;
 class StockRollBackModel extends StockModel
 {
     private $orderArray;
+    private $affectedRows = 0;
 
     public function __construct(array $orderArray)
     {
@@ -16,19 +17,25 @@ class StockRollBackModel extends StockModel
 
     public function stockRollBack()
     {
-        $sql1 = 'SELECT zone_id, zone_name, period, remain_period, order_no FROM `stock_log` WHERE ';
-        foreach ($this->orderArray as $orderNO) {
-            $sql1 .= '`order_no` = ? ' . 'OR ';
+        //历遍拼凑查询语句，使用生成器
+        $sql = 'SELECT zone_id, zone_name, period, remain_period, order_no FROM `stock_log` WHERE ';
+        foreach (self::generator() as $items) {
+            $sql .= $items;
         }
+        /*foreach ($this->orderArray as $orderNO) {
+             $sql1 .= '`order_no` = ? ' . 'OR ';
+        }*/
+
         //去掉SQL语句最后的‘ OR ’
-        $sql = substr($sql1, 0, -4);
+        $sql = substr($sql, 0, -4);
+
         //查询所有订单的库存记录，获得订单的节点ID、名称，购买月数，剩余月数，订单号
         $stockLogResult = DB::select($sql, $this->orderArray);
-        $affectedRows = 0;
         //历遍库存扣减记录，更新库存表剩余库存为 当前库存+订单扣减库存 ，从而实现库存还原
-        foreach ($stockLogResult as $key => $orderDetail) {
-            $affectedRows += DB::update('UPDATE `stock` SET `remain_period` = ? + `remain_period`  WHERE `zone_name` = ?',
+        foreach ($stockLogResult as $orderDetail) {
+            $this->affectedRows += DB::update('UPDATE `stock` SET `remain_period` = ? + `remain_period`  WHERE `zone_name` = ?',
                 [$orderDetail->period, $orderDetail->zone_name]);
+            //插入还原记录到库存操作记录表
             DB::table('stock_log')->insert([
                 'zone_id' => $orderDetail->zone_id,
                 'zone_name' => $orderDetail->zone_name,
@@ -39,6 +46,13 @@ class StockRollBackModel extends StockModel
                 'remark' => '订单取消，库存还原。操作人：' .  Session::get('user_id')
             ]);
         }
-        return $affectedRows;
+        return $this->affectedRows;
+    }
+
+    public function generator()
+    {
+        for ($i = 0; $i < count($this->orderArray); $i++) {
+            yield '`order_no` = ? ' . 'OR ';
+        }
     }
 }
--- app/Model/Order/UpdateOrderStatus.php
+++ app/Model/Order/UpdateOrderStatus.php
@@ -21,25 +21,52 @@ class UpdateOrderStatus extends Order
 
     public function userCancelOrder()
     {
-        $sql1 = 'UPDATE `order` SET `pay_status` = 2 WHERE ';
-        foreach ($this->data as $orderNO) {
+        if (empty($this->data)) {
+            return '请勾选订单进行操作';
+        }
+
+        //判断当前订单状态，若是已充值或者已取消则拒绝取消订单请求
+        $sqlForPayStatus = 'SELECT `order_no`, `pay_status` FROM `order` WHERE ';
+        foreach (self::generator(count($this->data), '`order_no` = ? ' . 'OR ') as $items) {
+            $sqlForPayStatus .= $items;
+        }
+        $sqlForPayStatus = substr($sqlForPayStatus, 0, -4);
+        $payStatusResult = DB::select($sqlForPayStatus, $this->data);
+        foreach ($payStatusResult as $tiems1) {
+            if ($tiems1->pay_status == 1 || $tiems1->pay_status == 2) {
+                return '订单' . $tiems1->order_no . '已充值或者已经取消，无法取消';
+            }
+        }
+
+        //更新订单的支付状态 0=>未充值 1=>已充值 2=>订单取消
+        /*foreach ($this->data as $orderNO) {
             //$sql1 .= '\'' . $orderNO . '\' ' . 'OR';
             $sql1 .= '`order_no` = ? ' . 'OR ';
+        }*/
+        $sqlForUpdateStatus = 'UPDATE `order` SET `pay_status` = 2 WHERE ';
+        foreach (self::generator(count($this->data), '`order_no` = ? ' . 'OR ') as $items2) {
+            $sqlForUpdateStatus .= $items2;
         }
-        //去掉SQL语句最后的‘ OR ’
-        $sql = substr($sql1, 0, -4);
-        //更新订单的支付状态 0=>未充值 1=>已充值 2=>取消订单
-        $affectedRows = DB::update($sql, $this->data);
-
+        //去掉SQL语句最后的' OR '
+        $sqlForUpdateStatus = substr($sqlForUpdateStatus, 0, -4);
+        $affectedRows = DB::update($sqlForUpdateStatus, $this->data);
         //return $affectedRows;
+
         //库存回滚，返回更新行数
         $rollBack = new StockRollBackModel($this->data);
         $rollBackRows = $rollBack->stockRollBack();
         if ($rollBackRows && $affectedRows) {
             return '取消成功';
         } else {
-            return '取消失败或者无操作';
+            return '取消失败';
         }
         //return $this->data;
+    }
+
+    public function generator($num, $string)
+    {
+        for ($i = 0; $i < $num; $i++) {
+            yield $string;
+        }
     }
 }
